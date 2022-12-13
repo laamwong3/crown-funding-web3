c449eafbf445436a0303127b787b9bd4db620e6b
Laam Wong <laamwong3@gmail.com>
2022-12-13 09:04:59 +0800

    create layout

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ConfigProvider, Layout, theme } from "antd";
+
+const { defaultSeed, darkAlgorithm, defaultAlgorithm } = theme;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Crown Funding Dapp</title>
+        <meta name="description" content="crown funding dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ConfigProvider theme={{ algorithm: darkAlgorithm }}>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </>
+  );
 }
