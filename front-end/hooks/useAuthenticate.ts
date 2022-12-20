4c2992552455f1450ceaeaf7befce734eb692312
Laam Wong <laamwong3@gmail.com>
2022-12-21 07:28:10 +0800

    add error message

--- front-end/hooks/useAuthenticate.ts
+++ front-end/hooks/useAuthenticate.ts
@@ -3,14 +3,33 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useAccount, useConnect, useDisconnect, useSignMessage } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { User } from ".";
+import { useNotification } from "../contexts/Notification";
 
 const useAuthenticate = () => {
-  const { connectAsync } = useConnect({ connector: new InjectedConnector() });
+  const { connectAsync } = useConnect({
+    connector: new InjectedConnector(),
+    onError: (error) => {
+      openNotification({
+        type: "error",
+        message: error.name,
+        description: error.message,
+      });
+    },
+  });
   const { disconnectAsync } = useDisconnect();
   const { isConnected } = useAccount();
-  const { signMessageAsync } = useSignMessage();
+  const { signMessageAsync } = useSignMessage({
+    onError: (error) => {
+      openNotification({
+        type: "error",
+        message: error.name,
+        description: error.message,
+      });
+    },
+  });
   const { data } = useSession();
   const { requestChallengeAsync } = useAuthRequestChallengeEvm();
+  const { openNotification } = useNotification();
 
   const authenticate = async () => {
     if (isConnected) {
@@ -26,6 +45,11 @@ const useAuthenticate = () => {
       });
 
       if (!challenge) {
+        openNotification({
+          type: "error",
+          message: "No Challenge Received",
+          description: "Please Re-login",
+        });
         throw new Error("No challenge received");
       }
       const signature = await signMessageAsync({ message: challenge.message });
