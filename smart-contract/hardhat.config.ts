6860af390233cb7396d649d87a24bac77818642d
Laam Wong <laamwong3@gmail.com>
2022-12-21 07:58:15 +0800

    deploy contract

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.7",
+  networks: {
+    testnet: {
+      url: process.env.RPC_NODE,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY,
+  },
 };
 
 export default config;
--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,6 +1,22 @@
 import { ethers } from "hardhat";
+import fs from "fs";
 
-async function main() {}
+async function main() {
+  const CrownFunding = await ethers.getContractFactory("CrownFunding");
+  const crownFunding = await CrownFunding.deploy();
+
+  const data = {
+    address: crownFunding.address,
+    abi: JSON.parse(crownFunding.interface.format("json").toString()),
+  };
+
+  fs.writeFileSync(
+    "../front-end/constants/contracts/CrownFunding.json",
+    JSON.stringify(data, null, 2)
+  );
+
+  console.log(`address is ${crownFunding.address}`);
+}
 
 main().catch((error) => {
   console.error(error);
