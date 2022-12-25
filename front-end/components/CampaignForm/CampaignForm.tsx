f99e236734c92b5e8a3d3d25af3283cb2dad506d
Laam Wong <laamwong3@gmail.com>
2022-12-25 16:39:14 +0800

    add upload form

--- front-end/components/CampaignForm/CampaignForm.tsx
+++ front-end/components/CampaignForm/CampaignForm.tsx
@@ -1,8 +1,35 @@
+import { Input, Typography } from "antd";
 import React from "react";
 import s from "./CampaignForm.module.scss";
+import UploadButton from "./UploadButton/UploadButton";
 
 const CampaignForm = () => {
-  return <div>CampaignForm</div>;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.form}>
+        <div className={s.form_item}>
+          <Typography.Title level={3}>Campaign Image</Typography.Title>
+          <UploadButton />
+        </div>
+        <div className={s.form_item}>
+          <Typography.Title level={3}>Title:</Typography.Title>
+          <Input.TextArea
+            placeholder="Enter a title of the campaign"
+            autoSize
+          />
+        </div>
+        <div className={s.form_item}>
+          <Typography.Title level={3}>Description:</Typography.Title>
+          <Input.TextArea
+            placeholder="Enter a description of the campaign"
+            autoSize={{ maxRows: 10, minRows: 5 }}
+            showCount
+            maxLength={1000}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CampaignForm;
