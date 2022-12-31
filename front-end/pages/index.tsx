ead601acddf641d4aa3917c4ee437617cea84e1e
Laam Wong <laamwong3@gmail.com>
2022-12-31 08:56:48 +0800

    minor test

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,8 +1,29 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAccount, useNetwork, useProvider, useSwitchNetwork } from "wagmi";
 
 const Home: NextPage = () => {
+  const { chain } = useNetwork();
+  const provider = useProvider();
+  const { connector } = useAccount();
+  const { pendingChainId, chains, data, switchNetwork } = useSwitchNetwork();
+  console.log(connector?.chains);
+
+  // console.log(pendingChainId);
+  // console.log(_network);
+  // connector?.on("change", (data) => {
+  //   if (data.chain) {
+  //     switchNetwork?.(data.chain.id);
+  //   }
+  // });
+  // useEffect(() => {
+  //   connector?.on("change", (data) => {
+  //     if (data.chain) {
+  //       switchNetwork?.(data.chain.id);
+  //     }
+  //   });
+  // }, [connector]);
   return (
     <div>
       <Link href={"/about"}>about</Link>
