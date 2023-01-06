2f7b80115a69e86ddbc41ed210bc82d1aae0fda8
Laam Wong <laamwong3@gmail.com>
2023-01-06 10:09:58 +0800

    add contract read

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,32 +1,86 @@
+import { BigNumber } from "ethers";
 import { NextPage } from "next";
-import Link from "next/link";
-import React, { useEffect } from "react";
-import { useAccount, useNetwork, useProvider, useSwitchNetwork } from "wagmi";
+import React, { useEffect, useState } from "react";
+import {
+  useContractInfiniteReads,
+  paginatedIndexesConfig,
+  useContractReads,
+} from "wagmi";
+import { abi, address } from "../constants/contracts/crownFunding";
+
+const crownFundingContract = {
+  abi,
+  address,
+};
 
 const Home: NextPage = () => {
-  const { chain } = useNetwork();
-  const provider = useProvider();
-  const { connector } = useAccount();
-  const { pendingChainId, chains, data, switchNetwork } = useSwitchNetwork();
-  console.log(connector?.chains);
+  const [items, setItems] = useState(0);
 
-  // console.log(pendingChainId);
-  // console.log(_network);
-  // connector?.on("change", (data) => {
-  //   if (data.chain) {
-  //     switchNetwork?.(data.chain.id);
-  //   }
-  // });
-  // useEffect(() => {
-  //   connector?.on("change", (data) => {
-  //     if (data.chain) {
-  //       switchNetwork?.(data.chain.id);
+  const { data: contractinfo, refetch } = useContractReads({
+    contracts: [
+      {
+        ...crownFundingContract,
+        functionName: "campignId",
+      },
+    ],
+  });
+
+  // console.log(contractinfo?.[0].toNumber());
+
+  const { data, hasNextPage, fetchNextPage } = useContractInfiniteReads({
+    cacheKey: "a",
+    contracts: (param = 0) => [
+      {
+        ...crownFundingContract,
+        functionName: "getCompaigns",
+        args: [BigNumber.from(param)] as const,
+      },
+    ],
+    getNextPageParam: (lastPage, allPages) => {
+      // console.log(allPages.length);
+      return allPages.length + 1;
+    },
+  });
+
+  console.log(data);
+  // const reload = async () => {
+  //   const result = await refetch();
+  //   // console.log(result.data?.[0].toNumber());
+  // };
+  // const { data, fetchNextPage } = useContractInfiniteReads({
+  //   cacheKey: "funding",
+  //   ...paginatedIndexesConfig(
+  //     (index) => [
+  //       {
+  //         ...crownFundingContract,
+  //         functionName: "getCompaigns",
+  //         args: [BigNumber.from(index)] as const,
+  //       },
+  //     ],
+  //     {
+  //       start: 0,
+  //       perPage: contractinfo?.[0].toNumber() ?? 0,
+  //       direction: "increment",
   //     }
-  //   });
-  // }, [connector]);
+  //   ),
+  // });
+  // console.log(contractinfo?.[0].toNumber());
+  // console.log(data?.pages[0][0]);
   return (
     <div>
-      <Link href={"/about"}>about</Link>
+      <button
+        onClick={async () => {
+          console.log(hasNextPage);
+          if (hasNextPage) {
+            await fetchNextPage();
+          }
+        }}
+      >
+        fetch more
+      </button>
+      {/* {data?.pages.map((d, index) => (
+        <p key={index}>{d[0].creator}</p>
+      ))} */}
     </div>
   );
 };
